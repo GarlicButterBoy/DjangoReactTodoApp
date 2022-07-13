7f524fefff2d3b1f40f254183a12de2dbfee4f86
Nick Sturch-Flint <nickdoesreviews@gmail.com>
2022-07-13 09:53:54 -0400

    git init

---
+++ .history/todo/views_20220713094934.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import TodoSerializer
+from .models import Todo
+
+# Create your views here.
+
+class TodoView(viewsets.ModelViewSet):
+    seriaz
